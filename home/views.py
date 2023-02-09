1f70fbbd6a0c6f935ce971540abb249b3ea8c27c
Arpit <arpitjaiswal9526@gmail.com>
2023-02-09 11:49:58 +0530

    Some bug fixes

--- home/views.py
+++ home/views.py
@@ -62,33 +62,34 @@ def search(request):
     return render(request, "home/search.html", params)
 
 def handleSignUp(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        Email = request.POST['email']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
+    if request.method=="POST":
+        # Get the post parameters
+        username=request.POST['username']
+        email=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
 
-        if len(username) > 15 or len(username) < 4:
-            messages.error(request, "Username must be under 10 character and must be greater than 4 character")
-            return redirect("home")
+        # check for errorneous input
+        if len(username)<10:
+            messages.error(request, " Your user name must be under 10 characters")
+            return redirect('home')
 
         if not username.isalnum():
-            messages.error(request, "Username should only contains letters and numbers")
-            return redirect("home")
-
-        if pass1 != pass2:
-            messages.error(request, "Password is not matched")
-            return redirect("home")
-
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('home')
+        
         # Create the user
-        myuser = User.objects.create_user(username, Email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name= fname
+        myuser.last_name= lname
         myuser.save()
-        messages.success(request, "Your iCoder account have been successfully created.")
-        return redirect("home")
+        messages.success(request, " Your iCoder has been successfully created")
+        return redirect('home')
 
     else:
         return HttpResponse("404 - Not Found")
